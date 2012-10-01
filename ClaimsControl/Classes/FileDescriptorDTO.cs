ab9fb086ce05492b0128ddf7d2a9368dce066969
gbukauskas@gmail.com <gbukauskas@gmail.com@00319c78-fc6d-970d-554a-cabb8df89a53>
2012-10-01 17:58:57 +0000

    Mano kodo atstatymas

---
+++ ClaimsControl/Classes/FileDescriptorDTO.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CC.Classes
+{
+    /// <summary>
+    /// <code>[dbo].[tblFileDescriptor]</code>
+    /// </summary>
+    [Serializable]
+    public class FileDescriptorDTO
+    {
+        /// <summary>
+        /// <code>[ID] [uniqueidentifier] NOT NULL,</code>
+        /// </summary>
+        public Guid ID { get; set; }
+
+        /// <summary>
+        /// <code>[MimeType] [nvarchar](50) NULL</code>
+        /// </summary>
+        public string MimeType { get; set; }
+	
+        /// <summary>
+        /// <code>[DateCreation] [datetime] NOT NULL</code>
+        /// </summary>
+        public DateTime DateCreation { get; set; }
+	
+        /// <summary>
+        /// <code>[DateModification] [datetime] NULL</code>
+        /// </summary>
+        public DateTime? DateModification { get; set; }
+	
+        /// <summary>
+        /// <code>[Icon] [image] NULL</code>
+        /// </summary>
+        public byte[] Icon { get; set; }
+	
+        /// <summary>
+        /// <code>[Owner] [uniqueidentifier] NOT NULL</code>
+        /// </summary>
+        public Guid Owner { get; set; }
+	
+        /// <summary>
+        /// <code>[IsPublic] [bit] NOT NULL</code>
+        /// </summary>
+        public bool IsPublic { get; set; }
+	
+        /// <summary>
+        /// <code>[Path] [nvarchar](max) NOT NULL</code>
+        /// </summary>
+        public string Path { get; set; }
+	
+        /// <summary>
+        /// <code>[Version] [timestamp] NOT NULL</code>
+        /// </summary>
+        public byte[] Version { get; set; }
+    }
+
+    [Serializable]
+    public class FileInfoDTO : FileDescriptorDTO
+    {
+        /// <summary>
+        /// <code>Binary content</code>
+        /// </summary>
+        public byte[] Content { get; set; }
+    }
+}
---
+++ ClaimsControl/Services/Interfaces/IFileManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CC.Classes;
+
+namespace CC.Services.Interfaces
+{
+    public interface IFileManager
+    {
+        public IQueryable<FileDescriptorDTO> GetFileDescriptors(Guid ownerId);
+    }
+}
