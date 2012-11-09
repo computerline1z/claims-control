d7d151a1671325bfa241db0e58163fcc8a0a9d2a
saulius.bruklys <saulius.bruklys@00319c78-fc6d-970d-554a-cabb8df89a53>
2012-11-09 09:59:14 +0000

    Filtro buttonų nustatymų patobulinimai

--- ClaimsControl/Scripts/My/MyPlugins.js
+++ ClaimsControl/Scripts/My/MyPlugins.js
@@ -170,18 +170,18 @@ jQuery(function ($) {
 (function( $ ){
 	//plugin buttonset vertical
 	$.fn.buttonsetv = function() {
-		$(':radio, :checkbox', this).wrap('<div style="margin: 1px"/>');
+		$(':radio, :checkbox', this).wrap('<div/>');// style="margin: 1px"
 		var ret=$(this).buttonset();
 		$('label:first', this).removeClass('ui-corner-left').addClass('ui-corner-top');
 		$('label:last', this).removeClass('ui-corner-right').addClass('ui-corner-bottom');
 		mw = 0; // max witdh
-		$('label', this).each(function(index){
-			w = $(this).width();
-			if (w > mw) mw = w; 
-		})
-		$('label', this).each(function(index){
-			$(this).width(mw);
-		})
+		// $('label', this).each(function(index){
+			// w = $(this).width();
+			// if (w > mw) mw = w; 
+		// })
+		// $('label', this).each(function(index){
+			// $(this).width(mw);
+		// })
 		return ret;
 	};
 })( jQuery );
